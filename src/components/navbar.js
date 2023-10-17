ecf3cd609a36de6c231cd272d59199a80858044b
NicolasOBP <nicolasobp15@gmail.com>
2023-10-16 21:13:47 -0300

    bugfixes: modal de currículo não aparecia no cel

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -227,22 +227,38 @@ export default function NavbarC(props) {
 
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <Disclosure.Button
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-blue-950 text-white"
-                      : "text-gray-900 hover:bg-gray-700 hover:text-white",
-                    "block rounded-md px-3 py-2 text-base font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  {item.name}
-                </Disclosure.Button>
-              ))}
+              {navigation.map((item) =>
+                item.name == "Cadastrar/Atualizar currículo" ? (
+                  <button
+                    onClick={() => setModalCu(true)}
+                    key={item.name}
+                    className={classNames(
+                      item.current
+                        ? "bg-blue-950 text-white"
+                        : "text-gray-900 hover:bg-gray-700 hover:text-white",
+                      "block rounded-md px-3 py-2 text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </button>
+                ) : (
+                  <Disclosure.Button
+                    key={item.name}
+                    as="a"
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? "bg-blue-950 text-white"
+                        : "text-gray-900 hover:bg-gray-700 hover:text-white",
+                      "block rounded-md px-3 py-2 text-base font-medium"
+                    )}
+                    aria-current={item.current ? "page" : undefined}
+                  >
+                    {item.name}
+                  </Disclosure.Button>
+                )
+              )}
             </div>
           </Disclosure.Panel>
         </>
